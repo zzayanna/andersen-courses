58931b0ebe8ecddc47e5610bcf7af95b9a705b7b
zzayanna <61212421+zzayanna@users.noreply.github.com>
2020-09-02 13:00:21 +0300

    changed the makeRequest() function a bit, replaced .then() with await

--- weather-api.js
+++ weather-api.js
@@ -4,39 +4,31 @@ const API_KEY = '9367469febbaa9db55123ddcbfe2badc';
 const URL = `api.openweathermap.org/data/2.5/weather`;
 
 class WeatherRequest {
-
   constructor(key, url) {
     this.key = key;
     this.url = url;
   }
 
-  makeRequest() {
-    return `https://${this.url}?appid=${this.key}`;
+  async makeRequest() {
+    try {
+      const response = await fetch(this.api_call);
+      const data = await response.json();
+      await (this.weatherData = data);
+    } catch (err) {
+      throw Error(err);
+    }
   }
-
 }
 
 class MyCityWeather extends WeatherRequest {
-
   constructor(key, url, city) {
     super(key, url);
     this.city = city;
   }
 
   async makeRequest() {
-    const ENDPOINT = super.makeRequest();
-    const API_CALL = `${ENDPOINT}&q=${this.city}&units=metric`;
-    try {
-      await fetch(API_CALL)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          this.weatherData = data;
-        })
-    } catch (err) {
-      throw Error(err);
-    }
+    this.api_call = `https://${this.url}?appid=${this.key}&q=${this.city}&units=metric`;
+    super.makeRequest();
   }
 }
 
@@ -44,33 +36,20 @@ const myCityWeather = new MyCityWeather(API_KEY, URL, 'Minsk');
 myCityWeather.makeRequest();
 
 class RandomCityWeather extends WeatherRequest {
-
   constructor(key, url) {
     super(key, url);
   }
 
   getRandomCity(cityList) {
-    let elemNum = Math.floor(Math.random() * cityList.length);
+    const elemNum = Math.floor(Math.random() * cityList.length);
     this.city = cityList[elemNum]['name'];
   }
 
   async makeRequest(list) {
     this.getRandomCity(list);
-    const ENDPOINT = super.makeRequest();
-    const API_CALL = `${ENDPOINT}&q=${this.city}&units=metric`;
-    try {
-      await fetch(API_CALL)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          this.weatherData = data;
-        });
-    } catch (err) {
-      throw Error(err);
-    }
+    this.api_call = `https://${this.url}?appid=${this.key}&q=${this.city}&units=metric`;
+    super.makeRequest();
   }
-
 }
 
 const randomCityWeather = new RandomCityWeather(API_KEY, URL);
